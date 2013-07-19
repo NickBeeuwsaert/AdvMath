bdcc64958fbbeff37b5bd77bfbe51c6a4f105906
r4z0rw0lf <nick.beeuwsaert@gmail.com>
2013-07-19 18:45:51 -0500

    woot woot scope support

--- scripts/advmath.js
+++ scripts/advmath.js
@@ -2,14 +2,36 @@
 (function(){
 	var expression = document.querySelector(".expression");
 	var log = document.querySelector(".log");
-	expression.addEventListener("keydown", function(evt){
-
+	var priorExpressions = [];
+	var expressionIndex = 0;
+	var currentExpression = undefined;
+	var scope = {};
+	scope.all_vars = function(){
+		var variables = [];
+		for(x in scope){
+			if(scope.hasOwnProperty(x)){
+				variables.push(x);
+			}
+		}
+		return variables;
+	};
+	scope.all_vars.toString = function() {
+		var ret_str = scope.all_vars().join(',');
+		return ret_str;
+	}
+	scope.all_vars.help_string = "Returns a list of all variables";
+	expression.addEventListener("keyup", function(evt){
+		console.log(evt.keyCode);
+		//up: 38, down: 40
 		if(evt.keyCode == 13){ // Enter...
 			var result = document.createElement("p");
 			var message = document.createElement("span");
-
+			priorExpressions.push(this.value);
+			currentExpression = undefined;
+			expressionIndex = 0;
 			try {
-				math_result = math.eval(this.value);
+				node = math.parse(this.value, scope);
+				math_result = node.eval();
 				result.appendChild(document.createTextNode(this.value));
 				message.appendChild(document.createTextNode(math_result));
 				result.appendChild(message);
@@ -19,9 +41,33 @@
 				message.appendChild(document.createTextNode(this.value));
 				result.appendChild(document.createTextNode(e.message+":"));
 				result.appendChild(message);
+				console.dir(e);
 			}
 			log.insertBefore(result, log.firstChild);
 
 		}
+		//Needs some work.
+		/*else if(evt.keyCode == 38) { // up
+			evt.preventDefault();
+			if(currentExpression == undefined){
+				currentExpression = this.value;
+			}
+			this.value = priorExpressions[expressionIndex>=priorExpressions.length?priorExpressions.length-1:expressionIndex++];
+		}else if(evt.keyCode == 40) { // down
+			evt.preventDefault();
+			if(currentExpression == undefined){
+				currentExpression = this.value;
+			}
+			if(expressionIndex > 0){
+				expressionIndex --;
+			}
+			if(expressionIndex == 0 && currentExpression != undefined){
+				this.value = currentExpression;
+				currentExpression = undefined;
+			}else{
+				this.value = priorExpressions[expressionIndex];
+			}
+
+		}*/
 	});
 })();
